b5a1bac2e59fc3f217e0fc7d3b185781be4ce492
osheep <osheep1337@gmail.com>
2016-01-15 08:21:24 +0000

    Create ha.sh

---
+++ ha.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##### Check root
+if[ ${EUID} -eq 0 ]; then
+  echo "Is root."
+else
+  echo "Not root, this script must be run as root"
+  exit 1
+fi
+
+##### Copy themes to manully config
+cp -n *.theme /usr/share/xfce4/terminal/colorschemes/
+
+exit 0
